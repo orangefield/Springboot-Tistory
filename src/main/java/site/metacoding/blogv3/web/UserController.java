edfaf2144cc3811ea945b2c049442aa7d24cbbff
field <jsw_777@naver.com>
2022-04-11 12:14:06 +0900

    회원가입, 로그인 페이지 완료

---
+++ src/main/java/site/metacoding/blogv3/web/UserController.java
@@ -0,0 +1,20 @@
+package site.metacoding.blogv3.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "/user/loginForm";
+    }
+
+    @GetMapping("/join-form")
+    public String joinForm() {
+        return "/user/joinForm";
+    }
+
+}
