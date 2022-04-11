bcb5b9b2d16682bac832eeac775af75504a1ed70
field <jsw_777@naver.com>
2022-04-11 10:10:17 +0900

    header, footer 완성

--- src/main/java/site/metacoding/blogv3/test/HelloController.java
+++ src/main/java/site/metacoding/blogv3/test/HelloController.java
@@ -1,12 +1,12 @@
 package site.metacoding.blogv3.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
         return "test";
     }
