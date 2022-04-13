0d5607a4b26fe34209d5e00eb25fd35259ec254e
field <jsw_777@naver.com>
2022-04-13 14:12:39 +0900

    [버그수정] username, password 체크 완료

--- src/main/java/site/metacoding/blogv3/web/UserController.java
+++ src/main/java/site/metacoding/blogv3/web/UserController.java
@@ -2,20 +2,18 @@ package site.metacoding.blogv3.web;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
-import site.metacoding.blogv3.domain.user.User;
-import site.metacoding.blogv3.domain.user.UserRepository;
 import site.metacoding.blogv3.handler.ex.CustomException;
 import site.metacoding.blogv3.service.UserService;
 import site.metacoding.blogv3.web.dto.user.JoinReqDto;
@@ -35,6 +33,12 @@ public class UserController {
     @GetMapping("/join-form")
     public String joinForm() {
         return "/user/joinForm";
+    }
+
+    @GetMapping("/api/user/username-same-check")
+    public ResponseEntity<?> usernameSameCheck(String username) {
+        boolean isNotSame = userService.유저네임중복체크(username); // true : 계속 진행
+        return new ResponseEntity<>(isNotSame, HttpStatus.OK);
     }
 
     @PostMapping("/join")
